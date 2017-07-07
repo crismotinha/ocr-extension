79ded964f75e04e303459d02a5bdeeb22326edf0
Cris Motinha <cristinamotinham@gmail.com>
2017-07-07 01:51:06 -0300

    Add first raw version of extension

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-function onExecuted(result) {
-  console.log(`We made it green`);
+const img = document.getElementsByTagName("IMG");
+
+if (img[0].src) {
+    const apiUrl = 'https://api.ocr.space/parse/imageurl';
+    const apikey = 'helloworld';
+    const imgUrl = img[0].src;
+    const queryString = apiUrl +'?apikey=' + apikey + '&url=' + imgUrl;
+    const myRequest = new Request(queryString, {method: 'GET'});
+
+    fetch(myRequest)
+    .then(function(response) { return response.json(); })
+        .then(function(data) {
+            const text = data.ParsedResults[0].ParsedText;
+            img[0].setAttribute('alt', text);
+       })
 }
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-
-function logTabs(tabs) {
-  for (let tab of tabs) {
-    // tab.url requires the `tabs` permission
-    console.log(tab.id, tab.url);
-  }
-}
-
-var querying = browser.tabs.query({});
-querying.then(logTabs, onError);
-
-var makeItGreen = 'document.body.style.border = "15px solid green"';
-
-var executing = browser.tabs.executeScript(5,{
-  code: makeItGreen
-});
-executing.then(onExecuted, onError);
